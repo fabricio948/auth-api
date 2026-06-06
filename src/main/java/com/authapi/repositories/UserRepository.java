98b1cc1da2735927617a8c4d20a685b598cd35dd
Fabricio <fabriciochelseablack@gmail.com>
2026-06-06 18:01:49 -0300

    feat: create UserRepository interface with findByEmail query method

---
+++ src/main/java/com/authapi/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.authapi.repositories;
+
+
+import com.authapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+import java.util.Optional;
+
+@Repository
+public class UserRepository extends JpaRepository<User , UUID> {
+
+    // Método customizado para encontrar um usuário pelo email
+    Optional<User> findByEmail(String email);
+}
